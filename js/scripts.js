35089e9c81d72c1769b7cc4ff9abbbdfee74c643
Cassandra Culkins and paul hess <cassy.culkins@gmail.com>
2016-01-21 10:03:17 -0800

    initialize repo

---
+++ js/scripts.js
@@ -0,0 +1,4 @@
+var switchLetter = function(str) {
+var regex1 = /[aeiouy]/g;
+  return str.replace(regex1, "-");
+}
---
+++ spec/specs.js
@@ -0,0 +1,5 @@
+describe("switchLetter", function() {
+   it("It should switch every vowel for a dash.", function() {
+     expect(switchLetter("switch your vowel for a dash.")).to.equal("sw-tch ---r v-w-l f-r - d-sh.");
+   });
+});

b09f097ca9da2a668b0632f6a0fbab098a98fac2
Cassandra Culkins and paul hess <cassy.culkins@gmail.com>
2016-01-21 11:35:02 -0800

    add front end functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,17 @@
+
 var switchLetter = function(str) {
 var regex1 = /[aeiouy]/g;
-  return str.replace(regex1, "-");
+ return str.replace(regex1, "-");
 }
+
+$(document).ready(function() {
+
+
+  $('form#forma').submit(function(event) {
+    var stringToPuzzle = $('input#word').val();
+    var puzzled = switchLetter(stringToPuzzle);
+    $('span.puzzle').text(puzzled);
+
+  event.preventDefault();
+});
+});
